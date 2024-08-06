27d63c2481d4eaa9aad8767494506993eb5fc4da
MUC_Mercury <alexander.kurakin@t-online.de>
2024-08-06 14:21:29 +0200

    __cplusplus preprocessor macro is added.

--- C++x_tmp/main.cpp
+++ C++x_tmp/main.cpp
@@ -10,5 +10,9 @@
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
+    // __cplusplus: This preprocessor macro provides a numeric value indicating
+    // the C++ standard. However, it's not always reliable and might not reflect
+    // the full capabilities of the compiler.
+    std::cout << "__cplusplus: " << __cplusplus << std::endl;
     return 0;
 }
