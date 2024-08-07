45e5fd00cbfeb52512258cf375ca49288933927a
MUC_Mercury <alexander.kurakin@t-online.de>
2024-08-07 10:08:14 +0200

    __cplusplus and __clang_version__ feature checking preprocessor macros are added.

--- C++x_tmp/main.cpp
+++ C++x_tmp/main.cpp
@@ -1,18 +1,19 @@
-//
-//  main.cpp
-//  C++x_tmp
-//
-//  Created by MUC-StarChild-Mercury on 06.08.24.
-//
+    //
+    //  main.cpp
+    //  C++x_tmp
+    //
+    //  Created by MUC-StarChild-Mercury on 06.08.24.
+    //
 
-#include <iostream>
+    #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    // __cplusplus: This preprocessor macro provides a numeric value indicating
-    // the C++ standard. However, it's not always reliable and might not reflect
-    // the full capabilities of the compiler.
-    std::cout << "__cplusplus: " << __cplusplus << std::endl;
-    return 0;
-}
+    int main(int argc, const char * argv[]) {
+        // insert code here...
+        std::cout << "Hello, World!\n\n";
+        // __cplusplus: This preprocessor macro provides a numeric value indicating
+        // the C++ standard. However, it's not always reliable and might not reflect
+        // the full capabilities of the compiler.
+        std::cout << "__cplusplus\t\t\t" << __cplusplus << std::endl;
+        std::cout << "__clang_version__\t" << __clang_version__ << std::endl;
+        return 0;
+    }
