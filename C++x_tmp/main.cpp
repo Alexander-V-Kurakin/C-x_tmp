2cf46cb1f52f612f214cb853ecb8c212463cfbfb
MUC_Mercury <alexander.kurakin@t-online.de>
2024-08-27 18:21:15 +0200

    Intermediate formatting

--- C++x_tmp/main.cpp
+++ C++x_tmp/main.cpp
@@ -1,19 +1,40 @@
-    //
-    //  main.cpp
-    //  C++x_tmp
-    //
-    //  Created by MUC-StarChild-Mercury on 06.08.24.
-    //
+//
+//  main.cpp
+//  C++x_tmp
+//
+//  Created by MUC-StarChild-Mercury on 06.08.24.
+//
 
-    #include <iostream>
+//  C++ access modifiers/specifiers:
+//
+//              All users   Derived classes     Class' own users/members
+//  Public          x               x                   x
+//  Protected                       x                   x
+//  Private                                             x
 
-    int main(int argc, const char * argv[]) {
-        // insert code here...
-        std::cout << "Hello, World!\n\n";
-        // __cplusplus: This preprocessor macro provides a numeric value indicating
-        // the C++ standard. However, it's not always reliable and might not reflect
-        // the full capabilities of the compiler.
-        std::cout << "__cplusplus\t\t\t" << __cplusplus << std::endl;
-        std::cout << "__clang_version__\t" << __clang_version__ << std::endl;
-        return 0;
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+using namespace std;
+
+#define TRACE(x) (std::cout << #x << " = " << x << std::endl)
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    char *cp = const_cast<char*>("Hello, World!\n");
+    std::cout << cp << std::endl;
+    // __cplusplus: This preprocessor macro provides a numeric value indicating
+    // the C++ standard. However, it's not always reliable and might not reflect
+    // the full capabilities of the compiler.
+//    TRACE(__cplusplus);
+//    TRACE(__clang_version__);
+    std::cout << "__cplusplus\t\t\t" << __cplusplus << std::endl;
+    std::cout << "__clang_version__\t" << __clang_version__ << std::endl << std::endl;
+    
+    {
+        
     }
+    
+    return 0;
+}
