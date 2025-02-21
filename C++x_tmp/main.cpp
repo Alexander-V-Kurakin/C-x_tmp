4d0e6dde8159782bb992ee7acdcf5e2d64600335
MUC_Mercury <alexander.kurakin@t-online.de>
2025-02-21 11:37:57 +0100

    I/O stream handling. File open, close, remove ops.

--- C++x_tmp/main.cpp
+++ C++x_tmp/main.cpp
@@ -13,6 +13,8 @@
 //  Private                                             x
 
 #include <iostream>
+#include <fstream>
+#include <cstdio>       // For std::remove
 #include <iomanip>
 #include <string>
 
@@ -32,9 +34,23 @@ int main(int argc, const char * argv[]) {
     std::cout << "__cplusplus\t\t\t" << __cplusplus << std::endl;
     std::cout << "__clang_version__\t" << __clang_version__ << std::endl << std::endl;
     
+    string in_file_name( "any_file_name" ), out_file_name( "any_file_name" );
+    
     {
+        ofstream out_file_stream( out_file_name );
         
+        if ( !out_file_stream.is_open())
+            cerr << "File " << out_file_name << " is not opened / created" << endl;
+        
+        ifstream in_file_stream( in_file_name );
+        
+        if ( !in_file_stream.is_open())
+            cerr << "File " << in_file_name << " is not opened" << endl;
+        
+        // The file stream automatically be closed once out of scope.
     }
+    
+    remove( out_file_name.c_str());     // Only required once, same file name used.
     
     return 0;
 }
